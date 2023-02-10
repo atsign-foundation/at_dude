5beb163b405dd1d128e51a795f18f1be248a0e00
Curtly Critchlow <curtlycritchlow@ccwc.io>
2023-02-10 10:34:59 -0400

    fix: select persona removed from the settings screen

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -9,7 +9,6 @@ import '../utils/texts.dart';
 import '../widgets/settings_button.dart';
 import '../widgets/switch_atsign.dart';
 import '../widgets/widgets.dart';
-import 'persona_screen.dart';
 
 class SettingsScreen extends StatelessWidget {
   const SettingsScreen({Key? key}) : super(key: key);
@@ -24,127 +23,93 @@ class SettingsScreen extends StatelessWidget {
         children: [
           const AppBackground(alignment: Alignment.bottomCenter),
           SafeArea(
-            child: SingleChildScrollView(
-              child: SizedBox(
-                height: MediaQuery.of(context).size.height,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Center(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Text(
-                            ContactService().currentAtsign,
-                            style: Theme.of(context)
-                                .textTheme
-                                .bodyText1!
-                                .copyWith(color: kPrimaryColor),
-                          ),
-                          Text(
-                            ContactService()
-                                    .loggedInUserDetails!
-                                    .tags!['name'] ??
-                                '',
-                            style: Theme.of(context).textTheme.headline3,
-                          ),
-                        ],
-                      ),
-                    ),
-                    Flexible(
-                      child: Container(
-                        width: 300,
-                        height: 72,
-                        decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(10)),
-                        child: Center(
-                          child: ListTile(
-                            leading: const Text(
-                              '👀',
-                              style: TextStyle(fontSize: 33),
-                            ),
-                            title: const Text(
-                              Texts.selectYourDude,
-                              style: TextStyle(fontSize: 18),
-                            ),
-                            trailing: const Icon(Icons.chevron_right_rounded),
-                            onTap: () {
-                              Navigator.of(context)
-                                  .pushNamed(PersonaScreen.routeName);
-                            },
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(10)),
-                          ),
-                        ),
-                      ),
-                    ),
-                    Flexible(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          SettingsButton(
-                            icon: Icons.bookmark_outline,
-                            title: 'Backup Your Keys',
-                            onTap: () {
-                              BackupKeyWidget(
-                                      atsign: ContactService().currentAtsign)
-                                  .showBackupDialog(context);
-                            },
-                          ),
-                          SettingsButton(
-                            icon: Icons.logout_rounded,
-                            title: Texts.switchAtsign,
-                            onTap: () async {
-                              await showModalBottomSheet(
-                                  context:
-                                      NavigationService.navKey.currentContext!,
-                                  builder: (context) =>
-                                      const AtSignBottomSheet());
-                            },
-                          ),
-                          const ResetAppButton(),
-                          SettingsButton(
-                            icon: Icons.help_center_outlined,
-                            title: Texts.faq,
-                            onTap: () async {
-                              final Uri _url =
-                                  Uri.parse('https://atsign.com/faqs/');
-                              if (!await launchUrl(_url)) {
-                                throw Exception('Could not launch $_url');
-                              }
-                            },
-                          ),
-                          SettingsButton(
-                            icon: Icons.forum_outlined,
-                            title: Texts.contactUs,
-                            onTap: () async {
-                              Uri _emailUri = Uri(
-                                scheme: 'mailto',
-                                path: 'atdude@atsign.com',
-                              );
-                              if (!await launchUrl(_emailUri)) {
-                                throw Exception('Could not launch $_emailUri');
-                              }
-                            },
-                          ),
-                          SettingsButton(
-                            icon: Icons.account_balance_wallet_outlined,
-                            title: Texts.privacyPolicy,
-                            onTap: () async {
-                              final Uri _url = Uri.parse(
-                                  'https://atsign.com/apps/atDude-privacy/');
-                              if (!await launchUrl(_url)) {
-                                throw Exception('Could not launch $_url');
-                              }
-                            },
-                          ),
-                        ],
-                      ),
-                    )
-                  ],
-                ),
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    ContactService().currentAtsign,
+                    style: Theme.of(context)
+                        .textTheme
+                        .bodyText1!
+                        .copyWith(color: kPrimaryColor),
+                  ),
+                  Text(
+                    ContactService().loggedInUserDetails!.tags!['name'] ?? '',
+                    style: Theme.of(context).textTheme.headline3,
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  SettingsButton(
+                    icon: Icons.bookmark_outline,
+                    title: 'Backup Your Keys',
+                    onTap: () {
+                      BackupKeyWidget(atsign: ContactService().currentAtsign)
+                          .showBackupDialog(context);
+                    },
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  SettingsButton(
+                    icon: Icons.logout_rounded,
+                    title: Texts.switchAtsign,
+                    onTap: () async {
+                      await showModalBottomSheet(
+                          context: NavigationService.navKey.currentContext!,
+                          builder: (context) => const AtSignBottomSheet());
+                    },
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  const ResetAppButton(),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  SettingsButton(
+                    icon: Icons.help_center_outlined,
+                    title: Texts.faq,
+                    onTap: () async {
+                      final Uri _url = Uri.parse('https://atsign.com/faqs/');
+                      if (!await launchUrl(_url)) {
+                        throw Exception('Could not launch $_url');
+                      }
+                    },
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  SettingsButton(
+                    icon: Icons.forum_outlined,
+                    title: Texts.contactUs,
+                    onTap: () async {
+                      Uri _emailUri = Uri(
+                        scheme: 'mailto',
+                        path: 'atdude@atsign.com',
+                      );
+                      if (!await launchUrl(_emailUri)) {
+                        throw Exception('Could not launch $_emailUri');
+                      }
+                    },
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  SettingsButton(
+                    icon: Icons.account_balance_wallet_outlined,
+                    title: Texts.privacyPolicy,
+                    onTap: () async {
+                      final Uri _url =
+                          Uri.parse('https://atsign.com/apps/atDude-privacy/');
+                      if (!await launchUrl(_url)) {
+                        throw Exception('Could not launch $_url');
+                      }
+                    },
+                  ),
+                ],
               ),
             ),
           )
