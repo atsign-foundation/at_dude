5058dc31ce35c8049642dd62582741b133441819
Curtly Critchlow <curtlycritchlow@ccwc.io>
2023-02-23 08:21:25 -0400

    fix:list view added to settings screen

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -27,131 +27,134 @@ class SettingsScreen extends StatelessWidget {
         children: [
           const AppBackground(alignment: Alignment.bottomCenter),
           SafeArea(
-            child: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Text(
-                    ContactService().currentAtsign,
-                    style: Theme.of(context)
-                        .textTheme
-                        .bodyLarge!
-                        .copyWith(color: kPrimaryColor),
-                  ),
-                  Text(
-                    ContactService().loggedInUserDetails!.tags!['name'] ?? '',
-                    style: Theme.of(context).textTheme.displaySmall,
-                  ),
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  SettingsButton(
-                    icon: Icons.block_outlined,
-                    title: 'Blocked Contacts',
-                    onTap: () {
-                      Navigator.of(context)
-                          .pushNamed(CustomBlockedScreen.routeName);
-                    },
-                  ),
-                  const SizedBox(
-                    height: 15,
-                  ),
-                  SettingsButton(
-                    icon: Icons.bookmark_outline,
-                    title: 'Backup Your Keys',
-                    onTap: () {
-                      BackupKeyWidget(atsign: ContactService().currentAtsign)
-                          .showBackupDialog(context);
-                    },
-                  ),
-                  const SizedBox(
-                    height: 15,
-                  ),
-                  SettingsButton(
-                    icon: Icons.logout_rounded,
-                    title: Texts.switchAtsign,
-                    onTap: () async {
-                      await showModalBottomSheet(
-                          context: NavigationService.navKey.currentContext!,
-                          builder: (context) => const AtSignBottomSheet());
-                    },
-                  ),
-                  const SizedBox(
-                    height: 15,
-                  ),
-                  const ResetAppButton(),
-                  const SizedBox(
-                    height: 15,
-                  ),
-                  SettingsButton(
-                    icon: Icons.help_center_outlined,
-                    title: Texts.faq,
-                    onTap: () async {
-                      final Uri _url = Uri.parse('https://atsign.com/faqs/');
-                      if (!await launchUrl(_url)) {
-                        throw Exception('Could not launch $_url');
-                      }
-                    },
-                  ),
-                  const SizedBox(
-                    height: 15,
-                  ),
-                  SettingsButton(
-                    icon: Icons.forum_outlined,
-                    title: Texts.contactUs,
-                    onTap: () async {
-                      Uri _emailUri = Uri(
-                        scheme: 'mailto',
-                        path: 'atdude@atsign.com',
-                      );
-                      if (!await launchUrl(_emailUri)) {
-                        throw Exception('Could not launch $_emailUri');
-                      }
-                    },
-                  ),
-                  const SizedBox(
-                    height: 15,
-                  ),
-                  SettingsButton(
-                    icon: Icons.account_balance_wallet_outlined,
-                    title: Texts.privacyPolicy,
-                    onTap: () async {
-                      final Uri _url =
-                          Uri.parse('https://atsign.com/apps/atDude-privacy/');
-                      if (!await launchUrl(_url)) {
-                        throw Exception('Could not launch $_url');
-                      }
-                    },
-                  ),
-                  const SizedBox(
-                    height: 15,
-                  ),
-                  SettingsButton(
-                    icon: Icons.delete_forever,
-                    title: Texts.deleteAllData,
-                    onTap: () async {
-                      SnackBars.notificationSnackBar(
-                        content: 'Are you sure',
-                        action: SnackBarAction(
-                            label: 'Yes',
-                            textColor: Colors.white,
-                            onPressed: () async {
-                              bool result = await context
-                                  .read<DudeController>()
-                                  .deleteAllData();
-                              if (result) {
-                                SnackBars.notificationSnackBar(
-                                    content: "All Dude Deleted");
-                              }
-                            }),
-                      );
-                    },
-                  )
-                ],
+              child: ListView(
+            children: [
+              Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      ContactService().currentAtsign,
+                      style: Theme.of(context)
+                          .textTheme
+                          .bodyLarge!
+                          .copyWith(color: kPrimaryColor),
+                    ),
+                    Text(
+                      ContactService().loggedInUserDetails!.tags!['name'] ?? '',
+                      style: Theme.of(context).textTheme.displaySmall,
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    SettingsButton(
+                      icon: Icons.block_outlined,
+                      title: 'Blocked Contacts',
+                      onTap: () {
+                        Navigator.of(context)
+                            .pushNamed(CustomBlockedScreen.routeName);
+                      },
+                    ),
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    SettingsButton(
+                      icon: Icons.bookmark_outline,
+                      title: 'Backup Your Keys',
+                      onTap: () {
+                        BackupKeyWidget(atsign: ContactService().currentAtsign)
+                            .showBackupDialog(context);
+                      },
+                    ),
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    SettingsButton(
+                      icon: Icons.logout_rounded,
+                      title: Texts.switchAtsign,
+                      onTap: () async {
+                        await showModalBottomSheet(
+                            context: NavigationService.navKey.currentContext!,
+                            builder: (context) => const AtSignBottomSheet());
+                      },
+                    ),
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    const ResetAppButton(),
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    SettingsButton(
+                      icon: Icons.help_center_outlined,
+                      title: Texts.faq,
+                      onTap: () async {
+                        final Uri _url = Uri.parse('https://atsign.com/faqs/');
+                        if (!await launchUrl(_url)) {
+                          throw Exception('Could not launch $_url');
+                        }
+                      },
+                    ),
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    SettingsButton(
+                      icon: Icons.forum_outlined,
+                      title: Texts.contactUs,
+                      onTap: () async {
+                        Uri _emailUri = Uri(
+                          scheme: 'mailto',
+                          path: 'atdude@atsign.com',
+                        );
+                        if (!await launchUrl(_emailUri)) {
+                          throw Exception('Could not launch $_emailUri');
+                        }
+                      },
+                    ),
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    SettingsButton(
+                      icon: Icons.account_balance_wallet_outlined,
+                      title: Texts.privacyPolicy,
+                      onTap: () async {
+                        final Uri _url = Uri.parse(
+                            'https://atsign.com/apps/atDude-privacy/');
+                        if (!await launchUrl(_url)) {
+                          throw Exception('Could not launch $_url');
+                        }
+                      },
+                    ),
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    SettingsButton(
+                      icon: Icons.delete_forever,
+                      title: Texts.deleteAllData,
+                      onTap: () async {
+                        SnackBars.notificationSnackBar(
+                          content: 'Are you sure',
+                          action: SnackBarAction(
+                              label: 'Yes',
+                              textColor: Colors.white,
+                              onPressed: () async {
+                                bool result = await context
+                                    .read<DudeController>()
+                                    .deleteAllData();
+                                if (result) {
+                                  SnackBars.notificationSnackBar(
+                                      content: "All Dude Deleted");
+                                }
+                              }),
+                        );
+                      },
+                    )
+                  ],
+                ),
               ),
-            ),
-          )
+            ],
+          ))
         ],
       ),
     );
