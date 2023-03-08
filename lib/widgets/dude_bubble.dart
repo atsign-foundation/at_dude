126ca1811046ee244040910a75de47a29450975a
Curtly Critchlow <curtlycritchlow@ccwc.io>
2023-03-08 09:34:19 -0400

    feat: UI show dude count playing and user can resend a dude after playing it

--- lib/widgets/dude_bubble.dart
+++ lib/widgets/dude_bubble.dart
@@ -1,5 +1,6 @@
 import 'dart:developer';
 
+import 'package:at_contacts_flutter/services/contact_service.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:just_audio/just_audio.dart';
@@ -8,18 +9,79 @@ import 'package:provider/provider.dart';
 import '../controller/controller.dart';
 import '../dude_theme.dart';
 import '../models/dude_model.dart';
+import '../screens/send_dude_screen.dart';
 import '../services/navigation_service.dart';
 import '../utils/enums.dart';
 import 'dude_card.dart';
 
-class DudeBubble extends StatelessWidget {
-  DudeBubble({
+class DudeBubble extends StatefulWidget {
+  const DudeBubble({
     Key? key,
     required this.dude,
   }) : super(key: key);
 
   final DudeModel dude;
+
+  @override
+  State<DudeBubble> createState() => _DudeBubbleState();
+}
+
+class _DudeBubbleState extends State<DudeBubble> with SingleTickerProviderStateMixin {
   final AudioPlayer audioPlayer = AudioPlayer();
+  late Animation<double> animation;
+  late AnimationController controller;
+  late Duration duration;
+  @override
+  void initState() {
+    if (widget.dude.selectedDudeType == DudeType.hi) {
+      audioPlayer.setAsset('assets/audios/hi_dude_scott.wav');
+      duration = const Duration(seconds: 2);
+    } else if (widget.dude.selectedDudeType == DudeType.youWontBelieve) {
+      audioPlayer.setAsset('assets/audios/you_woudnt_believe_dude_scott.wav');
+      duration = const Duration(seconds: 6);
+    } else if (widget.dude.selectedDudeType == DudeType.awesome) {
+      audioPlayer.setAsset('assets/audios/awesome_dude_scott.wav');
+      duration = const Duration(seconds: 2);
+    }
+
+    log('duration is' + duration.toString());
+    controller = AnimationController(vsync: this, duration: duration, upperBound: 315);
+
+    animation = Tween<double>(begin: 0, end: 315).animate(controller)
+      ..addListener(() {
+        controller.stop();
+        setState(() {});
+      });
+
+    controller.forward();
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  double convertDurationToWidth(
+      {required double currentDuration,
+      double durationMin = 0,
+      double widthMin = 0,
+      double widthMax = 315,
+      required double durationMax}) {
+    var durationRange = durationMax - durationMin;
+    double currentWidth = 0;
+    if (durationRange == 0) {
+      currentWidth = widthMin;
+    } else {
+      var widthRange = widthMax - widthMin;
+      currentWidth = (((currentDuration - durationMin) * widthRange) / durationRange) + widthMin;
+    }
+    log('new value is: ' + currentWidth.toString());
+    return currentWidth;
+  }
+
   String getAtsignName(String atsign) {
     var context = NavigationService.navKey.currentContext!;
     context.read<ContactsController>().getContacts();
@@ -43,58 +105,113 @@ class DudeBubble extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return DudeCard(
-      color: Colors.white,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Expanded(
-            child: Row(
-              children: [
-                IconButton(
-                    onPressed: () async {
-                      if (dude.selectedDudeType == DudeType.hi) {
-                        await audioPlayer.setAsset('assets/audios/hi_dude_scott.wav');
-                        await audioPlayer.play();
-                      } else if (dude.selectedDudeType == DudeType.youWontBelieve) {
-                        await audioPlayer.setAsset('assets/audios/you_woudnt_believe_dude_scott.wav');
-                        await audioPlayer.play();
-                      } else if (dude.selectedDudeType == DudeType.awesome) {
-                        await audioPlayer.setAsset('assets/audios/awesome_dude_scott.wav');
-                        await audioPlayer.play();
-                      }
+    log('controller value is:' + controller.value.toString());
+    log('audio player position is ${audioPlayer.position.inMilliseconds.toDouble()}');
 
-                      await Provider.of<DudeController>(context, listen: false).deleteDude(dude);
-                    },
-                    icon: const Icon(Icons.play_arrow_outlined)),
-                Flexible(
-                    child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      getAtsignName(dude.sender),
-                    ),
-                    Text(
-                      '"${dude.message[dude.selectedDudeType!.index].values.first}"',
-                      style: Theme.of(context).textTheme.bodyMedium!.copyWith(color: kPrimaryColor, fontSize: 12),
-                    )
-                  ],
-                )),
-              ],
-            ),
+    if (controller.value != 315) {
+      controller.animateTo(
+        convertDurationToWidth(
+            currentDuration: audioPlayer.position.inMilliseconds.toDouble(),
+            durationMax: duration.inMilliseconds.toDouble()),
+      );
+    }
+
+    return Stack(children: [
+      Positioned(
+        top: 20,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 4),
+          child: Container(
+            decoration: BoxDecoration(color: Colors.red, borderRadius: BorderRadius.circular(7)),
+            width: controller.value,
+            height: 50,
           ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              Text(
-                DateFormat.yMd().add_jm().format(dude.timeSent.toLocal()),
-                style: const TextStyle(fontSize: 10),
-              ),
-            ],
-          ),
-        ],
+        ),
       ),
-    );
+      DudeCard(
+        color: Colors.white,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            controller.value != 315
+                ? Expanded(
+                    child: Row(
+                      children: [
+                        IconButton(
+                            onPressed: () async {
+                              setState(() {});
+
+                              await audioPlayer.play();
+                              if (controller.value == 315) {
+                                // controller.reset();
+                              }
+
+                              // await showDialog(
+                              //     context: context,
+                              //     builder: (BuildContext context) => const AlertDialog(
+                              //           title: Text('Reply'),
+                              //           content: Text('Resend Dude'),
+                              //           actions: [],
+                              //         ));
+                            },
+                            icon: const Icon(Icons.play_arrow_outlined)),
+                        Flexible(
+                            child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              getAtsignName(widget.dude.sender),
+                            ),
+                            Text(
+                              '"${widget.dude.message[widget.dude.selectedDudeType!.index].values.first}"',
+                              style:
+                                  Theme.of(context).textTheme.bodyMedium!.copyWith(color: kPrimaryColor, fontSize: 12),
+                            )
+                          ],
+                        )),
+                      ],
+                    ),
+                  )
+                : Expanded(
+                    child: Row(
+                    children: [
+                      IconButton(
+                          onPressed: () {
+                            var atContact = ContactService()
+                                .contactList
+                                .firstWhere((element) => element.atSign == widget.dude.sender);
+                            Navigator.popAndPushNamed(context, SendDudeScreen.routeName, arguments: atContact);
+                          },
+                          icon: const Icon(Icons.reply)),
+                      Flexible(
+                          child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Send a dude back to',
+                            style: Theme.of(context).textTheme.bodyMedium!.copyWith(color: kPrimaryColor, fontSize: 12),
+                          ),
+                          Text(
+                            getAtsignName(widget.dude.sender) + ' ?',
+                          ),
+                        ],
+                      )),
+                    ],
+                  )),
+            controller.value != 315
+                ? Text(
+                    DateFormat.yMd().add_jm().format(widget.dude.timeSent.toLocal()),
+                    style: const TextStyle(fontSize: 10),
+                  )
+                : IconButton(
+                    onPressed: () async {
+                      await Provider.of<DudeController>(context, listen: false).deleteDude(widget.dude);
+                    },
+                    icon: const Icon(Icons.close)),
+          ],
+        ),
+      ),
+    ]);
   }
 }
--- lib/widgets/dude_card.dart
+++ lib/widgets/dude_card.dart
@@ -7,19 +7,22 @@ class DudeCard extends StatelessWidget {
     this.width = 343.0,
     this.height,
     this.color = const Color.fromRGBO(255, 255, 255, 74),
+    this.shadowColor = Colors.transparent,
   }) : super(key: key);
 
   final Widget child;
   final double width;
   final double? height;
   final Color color;
+  final Color shadowColor;
 
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Card(
         color: color,
-        shadowColor: Colors.transparent,
+        elevation: 2,
+        shadowColor: shadowColor,
         child: Container(
             padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 10),
             width: width,
