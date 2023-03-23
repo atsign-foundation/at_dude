2bfce6b11b9bdd0c01e5718c5675aaf86387ca26
Curtly Critchlow <curtlycritchlow@ccwc.io>
2023-03-23 10:08:04 -0400

    feat: notification implemented on android

--- lib/services/local_notification_service.dart
+++ lib/services/local_notification_service.dart
@@ -6,15 +6,16 @@ import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:timezone/data/latest.dart' as tz;
 import 'package:timezone/timezone.dart' as tz;
 
-import '../screens/notification_screen.dart';
+import '../models/arguments.dart';
+import '../utils/enums.dart';
+import '../widgets/widgets.dart';
 import 'navigation_service.dart';
 
 /// A singleton that controls all notifications for this app.
 ///
 ///
 class LocalNotificationService {
-  static final LocalNotificationService _notificationService =
-      LocalNotificationService._internal();
+  static final LocalNotificationService _notificationService = LocalNotificationService._internal();
 
   factory LocalNotificationService.getInstance() {
     return _notificationService;
@@ -24,8 +25,7 @@ class LocalNotificationService {
     return _notificationService;
   }
 
-  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
-      FlutterLocalNotificationsPlugin();
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
 
   LocalNotificationService._internal();
 
@@ -37,11 +37,13 @@ class LocalNotificationService {
       _requestIOSPermission();
     }
 
-    const AndroidInitializationSettings initializationSettingsAndroid =
-        AndroidInitializationSettings('@drawable/ic_stat_speaker_phone');
+    if (Platform.isAndroid) {
+      _requestAndroidPermission();
+    }
 
-    const DarwinInitializationSettings initializationSettingsDarwin =
-        DarwinInitializationSettings(
+    const AndroidInitializationSettings initializationSettingsAndroid = AndroidInitializationSettings('launcher_icon');
+
+    const DarwinInitializationSettings initializationSettingsDarwin = DarwinInitializationSettings(
       requestAlertPermission: false,
       requestBadgePermission: true,
       requestSoundPermission: true,
@@ -49,17 +51,15 @@ class LocalNotificationService {
     );
 
     const InitializationSettings initializationSettings =
-        InitializationSettings(
-            android: initializationSettingsAndroid,
-            iOS: initializationSettingsDarwin);
+        InitializationSettings(android: initializationSettingsAndroid, iOS: initializationSettingsDarwin);
 
     await flutterLocalNotificationsPlugin.initialize(
       initializationSettings,
       onDidReceiveNotificationResponse: (details) {
         switch (details.notificationResponseType) {
           case NotificationResponseType.selectedNotification:
-            Navigator.of(NavigationService.navKey.currentContext!)
-                .popAndPushNamed(NotificationScreen.routeName);
+            Navigator.of(NavigationService.navKey.currentContext!).popAndPushNamed(DudeNavigationScreen.routeName,
+                arguments: Arguments(route: Screens.notifications.index));
             break;
           case NotificationResponseType.selectedNotificationAction:
             break;
@@ -70,8 +70,7 @@ class LocalNotificationService {
 
   _requestIOSPermission() {
     flutterLocalNotificationsPlugin
-        .resolvePlatformSpecificImplementation<
-            IOSFlutterLocalNotificationsPlugin>()!
+        .resolvePlatformSpecificImplementation<IOSFlutterLocalNotificationsPlugin>()!
         .requestPermissions(
           alert: false,
           badge: true,
@@ -79,11 +78,16 @@ class LocalNotificationService {
         );
   }
 
+  _requestAndroidPermission() {
+    flutterLocalNotificationsPlugin
+        .resolvePlatformSpecificImplementation<AndroidFlutterLocalNotificationsPlugin>()!
+        .requestPermission();
+  }
+
   /// Shows notification when dude is sent to the current atsign.
   ///
   /// Notification currently only works in app on android.
-  Future<void> showNotifications(
-      int id, String title, String body, int seconds) async {
+  Future<void> showNotifications(int id, String title, String body, int seconds) async {
     await flutterLocalNotificationsPlugin.zonedSchedule(
       id,
       title,
@@ -94,7 +98,7 @@ class LocalNotificationService {
             channelDescription: 'Main channel  notifications',
             importance: Importance.max,
             priority: Priority.high,
-            icon: '@drawable/ic_stat_speaker_phone'),
+            icon: 'launcher_icon'),
         iOS: DarwinNotificationDetails(
           sound: 'default.wav',
           presentAlert: true,
@@ -102,8 +106,7 @@ class LocalNotificationService {
           presentSound: true,
         ),
       ),
-      uiLocalNotificationDateInterpretation:
-          UILocalNotificationDateInterpretation.wallClockTime,
+      uiLocalNotificationDateInterpretation: UILocalNotificationDateInterpretation.wallClockTime,
       androidAllowWhileIdle: true,
     );
   }
